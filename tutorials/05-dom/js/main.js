a8109cb3b796382db90a48aff34f591662382814
rinihimme <157917890+rinihimme@users.noreply.github.com>
2025-03-03 01:22:26 -0800

    followed tutorial

---
+++ tutorials/05-dom/js/main.js
@@ -0,0 +1,34 @@
+
+// console.log("ouioui");
+
+// let pageTitle = document.querySelector("#page-title");
+
+// // javascript time out changes h1 title after 2s
+// setTimeout(function(){
+//     pageTitle.style.color = "red";
+// } ,2000)
+
+// // clicking header changes bg colour
+// document.querySelector("#").onclick = function(){
+//     // console.log("clicked header");
+//     document.querySelector("body").style.backgroundColor = "yellow";
+// };
+
+document.querySelector("#classic").addEventListener("click", function(){
+    document.querySelector("#classic").style.visibility = "hidden";
+})
+document.querySelector("#dizzy").addEventListener("click", function(){
+    document.querySelector("#dizzy").style.visibility = "hidden";
+})
+document.querySelector("#sunshine").addEventListener("click", function(){
+    document.querySelector("#sunshine").style.visibility = "hidden";
+})
+document.querySelector("#natural").addEventListener("click", function(){
+    document.querySelector("#natural").style.visibility = "hidden";
+})
+document.querySelector("#greene").addEventListener("click", function(){
+    document.querySelector("#greene").style.visibility = "hidden";
+})
+document.querySelector("#invisible").addEventListener("click", function(){
+    document.querySelector("#invisible").style.visibility = "hidden";
+})
