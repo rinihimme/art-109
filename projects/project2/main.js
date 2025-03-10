d8127e991d71f92926b66e6e51c3467480701ea6
rinihimme <157917890+rinihimme@users.noreply.github.com>
2025-03-09 20:02:54 -0700

    uploaded project 2

---
+++ projects/project2/main.js
@@ -0,0 +1,36 @@
+
+document.querySelector("#frogai").addEventListener("click", function(){
+    document.querySelector("#frogai").style.visibility = "hidden";
+})
+
+document.querySelector("#kidsai").addEventListener("click", function(){
+    document.querySelector("#kidsai").style.visibility = "hidden"
+})
+
+document.querySelector("#fujiai").addEventListener("click", function(){
+    document.querySelector("#fujiai").style.visibility = "hidden"
+})
+
+document.querySelector("#egyptai").addEventListener("click", function(){
+    document.querySelector("#egyptai").style.visibility = "hidden"
+})
+
+document.querySelector("#skyai").addEventListener("click", function(){
+    document.querySelector("#skyai").style.visibility = "hidden"
+})
+
+document.querySelector("#cityai").addEventListener("click", function(){
+    document.querySelector("#cityai").style.visibility = "hidden"
+})
+
+document.querySelector("#hurricaneai").addEventListener("click", function(){
+    document.querySelector("#hurricaneai").style.visibility = "hidden"
+})
+
+document.querySelector("#kamalaai").addEventListener("click", function(){
+    document.querySelector("#kamalaai").style.visibility = "hidden"
+})
+
+document.querySelector("#trumpai").addEventListener("click", function(){
+    document.querySelector("#trumpai").style.visibility = "hidden"
+})
